595b1186c8719d2dfb1d32bdb72eee3a84e1b594
ekorolev <aksyline@gmail.com>
2019-03-22 15:05:05 +0300

    update public files

---
+++ src/public/precache-manifest.635604e8ec34c9e422000e05c652c5f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5f260c29f8621b79afdc",
+    "url": "/static/js/main.4999eb65.chunk.js"
+  },
+  {
+    "revision": "c1652f803518ffdadecb",
+    "url": "/static/js/2.0ac631b5.chunk.js"
+  },
+  {
+    "revision": "5f260c29f8621b79afdc",
+    "url": "/static/css/main.27c78ac2.chunk.css"
+  },
+  {
+    "revision": "c1652f803518ffdadecb",
+    "url": "/static/css/2.5ffe04f0.chunk.css"
+  },
+  {
+    "revision": "6bb4d0172ce6da5d7460ef8a46bb96d8",
+    "url": "/index.html"
+  }
+];
