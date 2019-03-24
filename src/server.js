c286a77e20aa7b7803016d733c120f4674467681
Evgenii <aksyline@gmail.com>
2019-03-24 18:45:44 +0300

    delete all public files which was built by client

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const Hapi = require('hapi')
 const mongoose = require('mongoose')
 const path = require('path')
-const inert = require('inert')
 const routes = require('./routes')
 const cors = require('hapi-cors')
 const Workers = require('./workers')
@@ -18,32 +17,15 @@ mongoose.connection.once('open', () => {
 const server = Hapi.server({
   port: process.env.PORT,
   host: process.env.HOST,
-  routes: {
-    files: {
-        relativeTo: path.join(__dirname, 'public')
-    }
-  }
 })
 
 const init = async () => {
-  await server.register(inert)
   await server.register({
     plugin: cors,
     options: {
       headers: ['Accept', 'Content-Type', 'Token']
     }
     
-  })
-  server.route(    {
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-      directory: {
-        path: '.',
-        redirectToSlash: true,
-        index: true,
-      }
-    }
   })
   server.route(routes)
 
